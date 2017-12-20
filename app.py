0fce65eb26e44c741543848d22a60075215fb1ae
maganaluis <maganaluis92@gmail.com>
2017-12-19 21:29:11 -0500

    renamed file to app.py

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
+import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 from flask import Flask, request, Response
-from flask import render_template, make_response
-import pandas as pd
+from flask import render_template
 import numpy as np
-import json, pickle
-import fastText as fasttext
 from fastText import load_model
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import io, base64
+import matplotlib.pyplot as plt
+import json
+import cPickle
+import io
+import base64
+
 
 class Classifier(object):
     def __init__(self):
+	self.encoder = cPickle.loads(open("encoder2.7.p", "rb").read())
         self.classifier = load_model('models/fasttext.bin')
-        self.encoder = pickle.load(open( "encoder.p","rb"))
 
     def predict(self, title):
         labels, values = self.classifier.predict(title, 10)
         labels = list(map(lambda x: int(x.strip('__label__')), labels))
         labels = self.encoder.inverse_transform(labels).tolist()
-        return dict(zip(*(labels,values)))
+        return dict(zip(*(labels, values)))
+
 
 app = Flask(__name__)
 classifier = Classifier()
 
+
 @app.route('/')
 def index():
     return render_template('classify.html')
+
 
 @app.route('/plot_chart', methods=['GET'])
 def plot_chart():
@@ -40,13 +49,14 @@ def plot_chart():
     plt.tight_layout()
     output = io.BytesIO()
     plt.savefig(output)
-    #response = make_response(output.getvalue())
-    #response.mimetype = 'image/png'
+    #  response = make_response(output.getvalue())
+    #  response.mimetype = 'image/png'
     plot_url = base64.b64encode(output.getvalue()).decode()
-    #https://stackoverflow.com/questions/20836766/how-do-i-remove-broken-image-box
+    #  https://stackoverflow.com/questions/20836766/how-do-i-remove-broken-image-box
     return render_template('classify.html', chart=plot_url)
 
-@app.route('/classify_this_post_api',methods=['POST'])
+
+@app.route('/classify_this_post_api', methods=['POST'])
 def classify_this_post_api():
     try:
         post_title = request.form['post_title']
@@ -57,6 +67,4 @@ def classify_this_post_api():
     return Response(js, status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    #don't use debug since it would create 2 schedulers due to reload functionality
-    #app.run(host="0.0.0.0", port=8019)
-    app.run(host='localhost',port=8080, debug=True)
+    app.run(debug=True,host='0.0.0.0')
